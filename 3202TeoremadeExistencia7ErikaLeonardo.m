a6a06fb9216ddc3c9ac200d51f1a729d33b51ed1
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-07 00:03:01 -0500

    Create 3202TeoremadeExistencia7ErikaLeonardo.m

---
+++ 3202TeoremadeExistencia7ErikaLeonardo.m
@@ -0,0 +1,21 @@
+% Octave Script
+% Title       : Teorema de Existencia
+% Description : Graficar las funciones y saber si existe integral
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 06 de Mayo 2021
+% Version     : 1
+% Notes       : funcion f(x)=1/1-(x^2), [-2,4]
+
+clear
+% Rango
+x=-2:0.1:4;
+% Valor de la funcion
+y=1./(1-x.^2);
+% Dibujar la funcion
+plot(x,y);
+% Titulo
+title("f(x)=1/1-(x^2)");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
