8b402d59dc5745cfe110e68f439b756ed6a17494
erileonardo <82676091+erileonardo@users.noreply.github.com>
2021-05-07 00:03:48 -0500

    Create 3202TeoremadeExistencia8ErikaLeonardo.m

---
+++ 3202TeoremadeExistencia8ErikaLeonardo.m
@@ -0,0 +1,21 @@
+% Octave Script
+% Title       : Teorema de Existencia
+% Description : Graficar las funciones y saber si existe integral
+% Author      : Erika Amairani Leonardo Arce
+% Date        : 06 de Mayo 2021
+% Version     : 1
+% Notes       : funcion f(x)=[x], [0,10]
+
+clear
+% Rango
+x=0:0.1:10;
+% Valor de la funcion
+y=[x];
+% Dibujar la funcion
+plot(x,y);
+% Titulo
+title("f(x)=[x]");
+% Etiqueta para x
+xlabel("X");
+% Etiqueta para y
+ylabel("Y");
